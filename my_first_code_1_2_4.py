d0455cdd43d5428583e014b71851bad0e8a9dae0
BurcuY <98539500+byaraman@users.noreply.github.com>
2022-03-26 03:42:53 +0000

    my_first_code for activit 1.2.4
    Write a comment  # this is my first line of code.
    Complete the following statements with your name and date, and run the code:
    *print('My name is ...')
    *print('The date is...')
    Enter another comment # Write a program to analyse real estate data in a new input box.
    Create variables for the sale prices. These are taken from the second column of the table. Your first variable will look like this a = 45000, were a is the Property ID and 45000 is its value. You will need to create variables for b, c, d, and e.
    Use various mathematical operators in Python, and the variables you have created, to:
    o	print the total sales from December. (Hint: Add all the sales together.)
    o	print the average sales price for the month of December. (Hint: Averages=TotalSales/Numberofsales.)
    o	print the property price for property C. (Hint: Salesprice−Comission)

---
+++ my_first_code_1_2_4.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This is my first line of code.
+
+
+# In[2]:
+
+
+print ( 'My name is Burcu')
+
+
+# In[3]:
+
+
+print ( 'The date is 25.03.2022')
+
+
+# In[5]:
+
+
+# Write a program to analyse real estate data
+
+
+# In[15]:
+
+
+a=45000 # property A _ sales price
+
+
+# In[16]:
+
+
+b=23400 # property B _ sales price
+
+
+# In[18]:
+
+
+c=67000 # property C _ sales price
+
+
+# In[10]:
+
+
+d=34600 # property D _ sales price
+
+
+# In[19]:
+
+
+e=12900 # property E _ sales price
+
+
+# In[20]:
+
+
+print ( a+b+c+d+e ) # total sales _ December
+
+
+# In[21]:
+
+
+print ( (a+b+c+d+e)/5 ) # average sales _ December
+
+
+# In[22]:
+
+
+print ( c-6500 ) # property C _ property price 
+
+
+# In[ ]:
+
+
+
+
